b0e486cd904afa56afe513e986986a185ecce53c
DanielChabrowski <dantezstudio@gmail.com>
2019-04-22 16:37:34 +0200

    Add sanitizers, refactor cmake

---
+++ cmake/build.cmake
@@ -0,0 +1,22 @@
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/arc)
+
+# Find includes in corresponding build directories
+set(CMAKE_INCLUDE_CURRENT_DIR ON)
+# Instruct CMake to run moc automatically when needed
+set(CMAKE_AUTOMOC ON)
+# Create code from a list of Qt designer ui files
+set(CMAKE_AUTOUIC ON)
+# Handle resources for Qt targets
+set(CMAKE_AUTORCC ON)
+
+add_compile_options(
+    -Werror
+    -Wextra
+    -Wpedantic
+)
