bb375f077bfdf6a9b69b1320dd40882fd2ad670f
Daniel Chabrowski <danchabrowski@gmail.com>
2021-12-31 20:25:48 +0100

    Update googletest tag

--- cmake/googletest-download.cmake
+++ cmake/googletest-download.cmake
@@ -6,7 +6,7 @@ include(ExternalProject)
 externalproject_add(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG master
+    GIT_TAG main
     SOURCE_DIR @GT_SOURCE_DIR@
     BINARY_DIR @GT_BUILD_DIR@
     CONFIGURE_COMMAND ""
